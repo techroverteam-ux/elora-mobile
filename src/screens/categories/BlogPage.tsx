e4848b8a4c2de2b25ccc9b04a3d1d92d6e490bc0
Neel Malvia <neelmalvia@rathi.com>
2025-07-30 13:03:10 +0530

    blog page done, added video player

--- src/screens/categories/BlogPage.tsx
+++ src/screens/categories/BlogPage.tsx
@@ -1,29 +1,140 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import AppBarHeader from '../../components/AppBarHeader'
+import { ScrollView, StyleSheet, Text, View, Dimensions } from 'react-native';
+import React from 'react';
+import AppBarHeader from '../../components/AppBarHeader';
 import { RouteProp, useRoute } from '@react-navigation/native';
+import FastImage from 'react-native-fast-image';
+import Video from 'react-native-video';
 
-// Define type for route params
-type BlogPage = {
-  BlogPage: { item: any };
+// Type Definitions
+type BlogItem = {
+  name: string;
+  description: string;
+  image: string;
 };
 
+type BlogPageRouteParams = {
+  BlogPage: { item: BlogItem };
+};
+
+const { width } = Dimensions.get('window');
+
 const BlogPage = () => {
-  const route = useRoute<RouteProp<BlogPage, 'BlogPage'>>();
+  const route = useRoute<RouteProp<BlogPageRouteParams, 'BlogPage'>>();
   const { item } = route.params;
 
-
   return (
-    <View>
-      <AppBarHeader title={item.name} />
+    <View style={styles.container}>
+      <AppBarHeader title={item?.name || 'Blog'} />
 
-      <View>
-        <Text>{JSON.stringify(item)}</Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <FastImage
+          style={styles.mainImage}
+          source={{
+            uri: item?.image,
+            headers: { Authorization: 'someAuthToken' },
+            priority: FastImage.priority.normal,
+          }}
+          resizeMode={FastImage.resizeMode.stretch}
+        />
+
+        <View style={styles.contentWrapper}>
+          <Text style={styles.title}>{item?.name}</Text>
+          <Text style={styles.description}>{item?.description}</Text>
+
+          <Text style={styles.paragraph}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            perferendis quidem, laborum in ducimus optio, deleniti numquam a
+            porro nam hic architecto ipsam vitae nemo esse cupiditate error
+            nihil ipsum?
+          </Text>
+
+          <View style={styles.videoContainer}>
+            <Video
+              source={{ uri: 'https://www.w3schools.com/html/mov_bbb.mp4' }}
+              style={styles.video}
+              controls
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text style={styles.paragraph}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+            perferendis quidem, laborum in ducimus optio, deleniti numquam a
+            porro nam hic architecto ipsam vitae nemo esse cupiditate error
+            nihil ipsum?
+          </Text>
+
+          <View style={styles.imageRow}>
+            {[...Array(2)].map((_, index) => (
+              <FastImage
+                key={index}
+                style={styles.sideImage}
+                source={{
+                  uri: item?.image,
+                  headers: { Authorization: 'someAuthToken' },
+                  priority: FastImage.priority.normal,
+                }}
+                resizeMode={FastImage.resizeMode.cover}
+              />
+            ))}
+          </View>
+        </View>
+      </ScrollView>
     </View>
-  )
-}
+  );
+};
 
-export default BlogPage
+export default BlogPage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  scrollContainer: {
+    paddingBottom: 100,
+  },
+  mainImage: {
+    width: '100%',
+    height: 200,
+  },
+  contentWrapper: {
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: '700',
+    color: '#F8803B',
+    marginBottom: 8,
+  },
+  description: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#333',
+    marginBottom: 12,
+  },
+  paragraph: {
+    fontSize: 14,
+    fontWeight: '400',
+    color: '#202020',
+    lineHeight: 20,
+    marginVertical: 10,
+  },
+  videoContainer: {
+    marginVertical: 20,
+  },
+  video: {
+    width: '100%',
+    aspectRatio: 16 / 9,
+  },
+  imageRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+  },
+  sideImage: {
+    width: (width - 48) / 2, // Considering 16px padding on both sides + 16px between
+    height: 200,
+    borderRadius: 8,
+  },
+});
